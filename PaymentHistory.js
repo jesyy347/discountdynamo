ede6bdb8e2888bb2b14a4a50a9ee8851bf66b0d4
jesyy347 <jessykavi29@gmail.com>
2025-03-13 21:13:10 +0530

    Create PaymentHistory.js

---
+++ PaymentHistory.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PaymentHistory = () => {
+  return (
+    <div className="p-5 text-gray-800">
+      <h2 className="text-xl font-bold mb-4">Payment History</h2>
+      <p>List of all previous transactions.</p>
+    </div>
+  );
+};
+
+export default PaymentHistory;
