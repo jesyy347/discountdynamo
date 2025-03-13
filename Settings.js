921f1efc9922ebd5c4796de153c05a62b9d02dbf
jesyy347 <jessykavi29@gmail.com>
2025-03-13 21:13:58 +0530

    Create Settings.js

---
+++ Settings.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Settings = () => {
+  return (
+    <div className="p-5 text-gray-800">
+      <h2 className="text-xl font-bold mb-4">App Settings</h2>
+      <p>Adjust your app preferences here.</p>
+    </div>
+  );
+};
+
+export default Settings;
