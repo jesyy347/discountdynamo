225fac4bd6e5e2587ef1037460e56405409ba774
jesyy347 <jessykavi29@gmail.com>
2025-03-13 21:17:12 +0530

    Create Profile.js

---
+++ Profile.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="p-5 text-gray-800">
+      <h2 className="text-xl font-bold mb-4">User Profile</h2>
+      <p>Name: John Doe</p>
+      <p>Email: johndoe@example.com</p>
+    </div>
+  );
+};
+
+export default Profile;
