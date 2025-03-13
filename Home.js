dad37901e93b5c44b3e0be0d97eee0f2d399a2d6
jesyy347 <jessykavi29@gmail.com>
2025-03-13 21:15:04 +0530

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="p-5 text-gray-800">
+      <h2 className="text-2xl font-bold mb-4">Welcome to Cashback App</h2>
+      <p>Select an option from the sidebar to get started.</p>
+    </div>
+  );
+};
+
+export default Home;
