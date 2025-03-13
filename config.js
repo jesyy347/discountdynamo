cde49881aeb7adfb92c50af33d41c45b2f46f0cb
jesyy347 <jessykavi29@gmail.com>
2025-03-13 18:35:49 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+  appName: "discountdynamo",
+  version: "1.0.0",
+  description: "A cashback system that earns real cashback only when a real payment occurs via UPI, scanner, or manual/automatic detection."
+};
+
+module.exports = config;
